67dec666ebf7d1a241a9763f2369187076586ccd
Sergey <dokxus@mail.ru>
2023-05-21 20:25:20 +0900

    Добавил картинки для Категорий медицинских товаров, для красивого отображения

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Medicine
 
 # Create your tests here.
+
+
+class TestMedicine(TestCase):
+    def test_init(self):
+        medicine = Medicine.objects.create(name='Ципрофлоксацин', price=100)
+        self.assertEqual(medicine.name, 'Ципрофлоксацин')
+
+    def test_str(self):
+        medicine = Medicine.objects.create(name='Ципрофлоксацин', price=100)
+        self.assertEqual(str(medicine), 'Ципрофлоксацин')
