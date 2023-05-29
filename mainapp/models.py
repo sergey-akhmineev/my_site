cfa2d8f58dcdd05aeb2a9dc6f15c5034ea20f9c0
Sergey <dokxus@mail.ru>
2023-05-29 13:56:01 +0900

    add

--- mainapp/models.py
+++ mainapp/models.py
@@ -8,6 +8,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 class Category(models.Model):
     name = models.CharField(max_length=32, unique=True)
+    img = models.ImageField(upload_to='category_images/',null=True)
 
     def __str__(self):
         return self.name
